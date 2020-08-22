da69ad321a3589e808b382cf3c90be2596c430c5
karim davoodi <karimdavoodi@gmail.com>
2020-08-22 20:45:52 +0430

    refactoring...

--- src/config.hpp
+++ src/config.hpp
@@ -1,10 +1,31 @@
+/*
+ * Copyright (c) 2020 Karim, karimdavoodi@gmail.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #pragma once
 
 #define MEDIA_ROOT  "/opt/sms/www/iptv/media/"
 #define HLS_ROOT    "/opt/sms/tmp/HLS/"
 #define FFMPEG      "/usr/bin/ffmpeg -v quiet "
 #define LOG(level) BOOST_LOG_TRIVIAL(level) << \
-                    "\033[0;32m[" << __func__ << ":" <<__LINE__ << "]\033[0m " 
+                    "\033[0;32m[" << __func__ << ":" <<__LINE__ << "]\033[0m "
 
 const int PKT_SIZE = 1316;
 const int INPUT_PORT = 3200;
